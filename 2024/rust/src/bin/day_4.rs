ac7cc125ed22c8595370e32b453accdeb4c6cb5b
coffee-compiler <coffee-compiler@vrapped.de>
2024-12-04 20:02:41 +0100

    solve day4 puzzle2

--- 2024/rust/src/bin/day_4.rs
+++ 2024/rust/src/bin/day_4.rs
@@ -1,14 +1,16 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 fn main() {
     let data = fs::read_to_string("assets/day_4.txt").unwrap();
 
-    let result_p1 = word_search(&data);
+    let result_p1 = xmas_word_search(&data);
+    let result_p2 = mas_word_search(&data);
 
     println!("{}", result_p1);
+    println!("{}", result_p2);
 }
 
-fn word_search(data: &str) -> i32 {
+fn xmas_word_search(data: &str) -> i32 {
     let mut total = 0;
     let mut chars_in_lines: Vec<Vec<char>> = Vec::new();
 
@@ -98,6 +100,55 @@ fn word_search(data: &str) -> i32 {
                 {
                     total += 1;
                 }
+            }
+        }
+    }
+
+    total
+}
+
+fn mas_word_search(data: &str) -> i32 {
+    let mut total = 0;
+    let mut chars_in_lines: Vec<Vec<char>> = Vec::new();
+
+    for line in data.lines() {
+        chars_in_lines.push(line.chars().collect());
+    }
+
+    let line_count = chars_in_lines.len();
+
+    // always the same
+    let line_char_count = chars_in_lines[0].len();
+
+    let mut char_counter = HashMap::new();
+    char_counter.insert('X', 0);
+    char_counter.insert('M', 0);
+    char_counter.insert('A', 0);
+    char_counter.insert('S', 0);
+
+    for i in 1..line_count - 1 {
+        for j in 1..line_char_count - 1 {
+            if chars_in_lines[i][j] != 'A' {
+                continue;
+            }
+
+            *char_counter.get_mut(&'X').unwrap() = 0;
+            *char_counter.get_mut(&'M').unwrap() = 0;
+            *char_counter.get_mut(&'A').unwrap() = 0;
+            *char_counter.get_mut(&'S').unwrap() = 0;
+
+            let top_left = &chars_in_lines[i - 1][j - 1];
+            let top_right = &chars_in_lines[i - 1][j + 1];
+            let bottom_left = &chars_in_lines[i + 1][j - 1];
+            let bottom_right = &chars_in_lines[i + 1][j + 1];
+
+            *char_counter.get_mut(top_left).unwrap() = char_counter[top_left] + 1;
+            *char_counter.get_mut(top_right).unwrap() = char_counter[top_right] + 1;
+            *char_counter.get_mut(bottom_left).unwrap() = char_counter[bottom_left] + 1;
+            *char_counter.get_mut(bottom_right).unwrap() = char_counter[bottom_right] + 1;
+
+            if char_counter[&'M'] == 2 && char_counter[&'S'] == 2 && top_left != bottom_right {
+                total += 1;
             }
         }
     }
